9a48d20098da5cb1d1ed71c009d27369a72a30db
Yubin <103750003+Oiya861028@users.noreply.github.com>
2023-10-28 22:10:05 -0400

    Update: Use parseInt to check if value is valid

--- main.java
+++ main.java
@@ -23,34 +23,28 @@ public class main{
 
     public static boolean validValue(String base, String value){
         if(base.equals("Decimal")){
-            for(int i=0;i<value.length();i++){
+            try {
+                int num = Integer.parseInt(value);
+            } catch (Exception e) {
+                return false;
+            }
+            return true;
+        } 
+        else if (base.equals("Binary")){        
+            for(int i=0;i<value.length();i++) {
                 switch (value.charAt(i)) {
-                    case '1':
-                        break;
-                    case '2':
-                        break;
-                    case '3':
-                        break;
-                    case '4':
-                        break;
-                    case '5':
-                        break;
-                    case '6':
-                        break;
-                    case '7':
-                        break;
-                    case '8':
-                        break;
-                    case '9':
-                        break;
                     case '0':
+                        break;
+                    case '1':
                         break;
                     default:
                         return false;
                 }
             }
-            return true;
         }
-        return true;
+        else {
+
+        }
+        return false;
     }
 }
