e32d667f3ca4d5e7d6be5d43060c25f67231e0e3
Yubin <103750003+Oiya861028@users.noreply.github.com>
2023-10-28 22:00:01 -0400

    Add value and test validity for decimal

--- main.java
+++ main.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
 public class main{
-    public static Scanner sc = new Scanner(System.in);
+    public static final Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
+        //Takes in base from user and checks if it's valid
         System.out.println("Enter your numbers base:");
         String initialBase = sc.nextLine();
         boolean validBase=false; 
@@ -12,7 +13,44 @@ public class main{
                 initialBase = sc.nextLine();
             }
         }
-        System.out.println("Completed");
 
+        //Take in a value of the user and check if it matches the base
+        System.out.println("Enter the value you want to convert:");
+        String value = sc.nextLine();
+        System.out.println(validValue(initialBase,value));
+
+    }
+
+    public static boolean validValue(String base, String value){
+        if(base.equals("Decimal")){
+            for(int i=0;i<value.length();i++){
+                switch (value.charAt(i)) {
+                    case '1':
+                        break;
+                    case '2':
+                        break;
+                    case '3':
+                        break;
+                    case '4':
+                        break;
+                    case '5':
+                        break;
+                    case '6':
+                        break;
+                    case '7':
+                        break;
+                    case '8':
+                        break;
+                    case '9':
+                        break;
+                    case '0':
+                        break;
+                    default:
+                        return false;
+                }
+            }
+            return true;
+        }
+        return true;
     }
 }
